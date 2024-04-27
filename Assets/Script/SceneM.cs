e607b43c248ac33bc334a29d112913d7a913bcb6
Tartoruz <131382498+Tartoruz@users.noreply.github.com>
2024-04-27 18:47:55 +0700

    Add sound and how to play

--- Assets/Script/SceneM.cs
+++ Assets/Script/SceneM.cs
@@ -6,33 +6,43 @@ using UnityEngine.UI;
 
 public class SceneM : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource audio;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     public void GameScene()
     {
         SceneManager.LoadSceneAsync(1);
     }
+
     public void MainMenu()
     {
         SceneManager.LoadSceneAsync(0);
     }
+
     public void Quit()
     {
         Application.Quit();
     }
+
     public void EndCredits()
     {
         SceneManager.LoadSceneAsync(3);
     }
+
+    public void PlayButton()
+    {
+        audio.Play();
+    }
+
 }
 
